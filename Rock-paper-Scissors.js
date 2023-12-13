8a808924f288ac1612e83df2370c640e3cdda5e2
hibafatima05 <146421108+hibafatima05@users.noreply.github.com>
2023-12-12 21:50:16 -0700

    Added Instructions and comments

--- Rock-paper-Scissors.js
+++ Rock-paper-Scissors.js
@@ -1,23 +1,39 @@
+
+// Retrieve the score from localStorage 
 let score = JSON.parse(localStorage.getItem('score')) ||{
   Wins:0,
   losses:0,
   Ties : 0
 };     
 
+// Update the score element on the UI
+
 updateScoreElement();
+
+// Function to play the game based on the player's move
 
 function playGame(playerMove){
 
-const computerMove = pickComputerMove();
+const computerMove = pickComputerMove(); // Generate a random move for the computer
+
     let Result = '';
+    
+  // Determine the result of the game based on player's move and computer's move
+  // Check various outcomes based on player's move
+    
 
 if(playerMove ==='SCISSORS'){
 
 if(computerMove === 'ROCK'){
+
 Result = 'you loose';
-} else if (computerMove === 'PAPER'){
-Result = 'you WON!!';
-} else if(computerMove === 'SCISSORS'){
+
+} 
+else if (computerMove === 'PAPER'){
+ 
+  Result = 'you WON!!';
+} 
+else if(computerMove === 'SCISSORS'){
 
 Result ='tie';
 }
@@ -25,10 +41,14 @@ Result ='tie';
 }  else if(playerMove=== 'PAPER'){
 
 if(computerMove === 'ROCK'){
+
 Result = 'you WON!!';
-} else if (computerMove === 'PAPER'){
+} 
+else if (computerMove === 'PAPER'){
+
 Result = 'tie';
-} else if(computerMove === 'SCISSORS'){
+} 
+else if(computerMove === 'SCISSORS'){
 
 Result ='you loose';
 }
@@ -37,14 +57,19 @@ Result ='you loose';
 {
 
 if(computerMove === 'ROCK'){
+
 Result = 'tie';
-} else if (computerMove === 'PAPER'){
+} 
+else if (computerMove === 'PAPER'){
+
 Result = 'you loose';
 } else if(computerMove === 'SCISSORS'){
 
 Result ='you WON!!';
 }
 }
+
+  // Update the scores based on the game result
 
 if (Result === 'you WON!!') {
 score.Wins +=1;
@@ -53,15 +78,20 @@ score.losses += 1;
 } else if (Result === 'tie') {
 score.Ties += 1;
 }
-localStorage.setItem('score',JSON.stringify(score));
+  // Store the updated score in localStorage
+
+localStorage.setItem('score',JSON.stringify(score)); 
+
+  // Update the score element 
 
 updateScoreElement();
 
+  // Display the result and moves on the UI
 
 document.querySelector('.js-result').innerHTML = Result;       
 document.querySelector('.js-moves').innerHTML = `You <img src="
-${playerMove}.jpeg" class="move-icon">
-<img src="${computerMove}.jpeg"class="move-icon">
+${playerMove}.png" class="move-icon">
+<img src="${computerMove}.png"class="move-icon">
 Computer`;
 
 // alert(`you picked ${playerMove}..computer picked ${computerMove}.${Result}
@@ -69,11 +99,16 @@ Computer`;
 
 }
 
+// Function to update the score element on the UI
+
 function updateScoreElement(){
 
 document.querySelector('.js-score').innerHTML = `Wins : ${score.Wins}, Losses: ${score.losses}, Ties : ${score.Ties}`;
 
 }            
+
+
+// Function to randomly pick a move for the computer
 
 function pickComputerMove(){
         const randomNumber= Math.random();
